7930ccab1d3f76013fc7a74a836c88303b926fcd
hahaanisha <168823401+hahaanisha@users.noreply.github.com>
2025-06-09 22:04:51 +0530

    add/remove button

--- Frontend/src/Components/FoodItem/FoodItem.jsx
+++ Frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,13 +1,25 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React,{useState} from 'react'
 import { assets } from '../../assets/assets'
 import './FoodItem.css'
 
 const FoodItem = ({id,name,price,description,image}) => {
+    const [itemCount, setItemCount] =  useState(0)
   return (
     <div className='food-item'>
         <div className='food-item-image-container'>
             <img className="food-item-image" src={image} alt="" />
+            { //ternary operator
+                !itemCount 
+                ? <img className='add' onClick={()=>{setItemCount(prev=>prev+1)}} src={assets.add_icon_white}></img> 
+                : <div className='food-item-counter'>
+                 <img onClick={()=>{setItemCount(prev=>prev-1)}} src={assets.remove_icon_red} alt="" />
+                 <p>{itemCount}</p>
+                 <img onClick={()=>{setItemCount(prev=>prev+1)}} src={assets.add_icon_green} alt="" />
+                </div>
+            }
+
+
         </div>
         <div className='food-item-info'>
             <div className='food-item-name-rating'>
