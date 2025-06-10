295678237e822f3c6cf85c43409099d3af4a8cb6
hahaanisha <168823401+hahaanisha@users.noreply.github.com>
2025-06-10 16:38:16 +0530

    links updated

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -3,14 +3,25 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className='header' >
-      <div className='header-contents'>
-        <h2 className='text-7xl font-medium text-amber-50 font-outfit'>Order your favourite food here</h2>
-        <p className='text-amber-50 text-[20px]'>Forkit makes food delivery effortless and fast. Browse through a variety of local restaurants, customize your meals, track your order in real-time, and have delicious food delivered straight to your doorstep.</p>
-        <button className='border-0 border-amber-50 font-medium bg-amber-50 text-2xl rounded-[30px] py-[1vw] px-[2.3vw] '>View Menu</button>
+    <div className="header">
+      <div className="header-contents">
+        <h2 className="text-7xl font-medium text-amber-50 font-outfit">
+          Order your favourite food here
+        </h2>
+        <p className="text-amber-50 text-[20px]">
+          Forkit makes food delivery effortless and fast. Browse through a
+          variety of local restaurants, customize your meals, track your order
+          in real-time, and have delicious food delivered straight to your
+          doorstep.
+        </p>
+        <a href="#explore-menu">
+          <button className="border-0 border-amber-50 font-medium bg-amber-50 text-2xl rounded-[30px] py-[1vw] px-[2.3vw] cursor-pointer ">
+            View Menu
+          </button>
+        </a>
       </div>
     </div>
-  )
+  );
 }
 
 export default Header
--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -12,25 +12,82 @@ const Navbar = ({setShowLogin}) => {
     const {getTotalCartAmount} = useContext(StoreContext)
 
   return (
-    <div className='navbar mt-5 py-5 flex justify-between items-center'>
-        <Link to='/'><img className="logo w-[150px]  h-auto object-contain" src={assets.logo} alt="" /></Link>
-        <ul className='navbar-menu flex list-none gap-[20px] text-2xl text-gray-700 font-outfit'>
-            <li onClick={()=>setMenu("home")} className={menu === "home" ? "active":""}>home</li>
-            <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active":""}>menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app" ? "active":""}>mobile-app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us" ? "active":""}>contact us</li>
-        </ul>
-        <div className='navbar-right flex items-center gap-10'>
-            <img src={assets.search_icon} alt="" />
-            <div className='navbar-search-icon'>
-                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
-                <div className={getTotalCartAmount()===0 ? "" : "dot"}></div>
-            </div>
-            <button onClick={()=>setShowLogin(true)} className='bg-transparent text-2xl text-gray-700 border border-gray-700 rounded-4xl py-3 px-8 hover:bg-amber-700 hover:text-amber-50'> Sign in </button>
+    <div className="navbar mt-5 py-5 flex justify-between items-center">
+      <Link to="/">
+        <img
+          className="logo w-[150px]  h-auto object-contain"
+          src={assets.logo}
+          alt=""
+        />
+      </Link>
+      <ul className="navbar-menu flex list-none gap-[20px] text-2xl text-gray-700 font-outfit">
+        <li
+          onClick={() => {
+            setMenu("home");
+            document
+              .getElementById("home")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </li>
 
+        <li
+          onClick={() => {
+            setMenu("menu");
+            document
+              .getElementById("explore-menu")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </li>
+
+        <li
+          onClick={() => {
+            setMenu("mobile-app");
+            document
+              .getElementById("app-download")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </li>
+
+        <li
+          onClick={() => {
+            setMenu("contact-us");
+            document
+              .getElementById("footer")
+              ?.scrollIntoView({ behavior: "smooth" });
+          }}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </li>
+      </ul>
+
+      <div className="navbar-right flex items-center gap-10">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
+        <button
+          onClick={() => setShowLogin(true)}
+          className="bg-transparent text-2xl text-gray-700 border border-gray-700 rounded-4xl py-3 px-8 hover:bg-amber-700 hover:text-amber-50"
+        >
+          {" "}
+          Sign in{" "}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
