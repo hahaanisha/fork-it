1751de9846e1a2897506b3b4a6e3782b48b1fdbe
hahaanisha <168823401+hahaanisha@users.noreply.github.com>
2025-06-10 15:51:21 +0530

    Cart page

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable no-unused-vars */
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 import {assets} from '../../assets/assets'
 import './Navbar.css'
+import '../../Pages/Cart/Cart'
 
 const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home")
 
   return (
     <div className='navbar mt-5 py-5 flex justify-between items-center'>
-        <img className="logo w-[150px]  h-auto object-contain" src={assets.logo} alt="" />
+        <Link to='/'><img className="logo w-[150px]  h-auto object-contain" src={assets.logo} alt="" /></Link>
         <ul className='navbar-menu flex list-none gap-[20px] text-2xl text-gray-700 font-outfit'>
             <li onClick={()=>setMenu("home")} className={menu === "home" ? "active":""}>home</li>
             <li onClick={()=>setMenu("menu")} className={menu === "menu" ? "active":""}>menu</li>
@@ -18,7 +20,7 @@ const Navbar = ({setShowLogin}) => {
         <div className='navbar-right flex items-center gap-10'>
             <img src={assets.search_icon} alt="" />
             <div className='navbar-search-icon'>
-                <img src={assets.basket_icon} alt="" />
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className='dot'></div>
             </div>
             <button onClick={()=>setShowLogin(true)} className='bg-transparent text-2xl text-gray-700 border border-gray-700 rounded-4xl py-3 px-8 hover:bg-amber-700 hover:text-amber-50'> Sign in </button>
--- Frontend/src/Pages/Cart/Cart.jsx
+++ Frontend/src/Pages/Cart/Cart.jsx
@@ -1,8 +1,80 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext';
+import { food_list } from '../../assets/assets';
+import './Cart.css'
 
 const Cart = () => {
+
+  const { cartItem, addToCart, removeFromCart } = useContext(StoreContext);
+
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className='cart-items'>
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+<hr />
+
+{food_list.map((item,index)=>{
+
+  if(cartItem[item._id]>0){
+    return (
+      <div>
+        <div className='cart-items-title cart-items-item'>
+        <img src={item.image} alt="" />
+        <p>{item.name}</p>
+        <p>₹{item.price*100}</p>
+        <p>{cartItem[item._id]}</p>
+        <p>₹{item.price*100*cartItem[item._id]}</p>
+        <p onClick={()=> removeFromCart(item._id)} className='cross'>X</p>
+      </div>
+      <hr />
+      </div>
+      
+    )
+  }
+})}
+      </div>
+<div className="cart-bottom">
+  <div className="cart-total">
+    <h2>Cart Total</h2>
+    <div>
+      <div className="cart-total-details">
+        <p>Subtotal</p>
+        <p>{0}</p>
+      </div>
+      <hr />
+      <div className="cart-total-details">
+        <p>Delivery Fee</p>
+        <p>{2}</p>
+      </div>
+      <hr />
+      <div className="cart-total-details">
+        <b>Total</b>
+        <b>{0}</b>
+      </div>
+    </div>
+    <button>PROCEED TO CHECKOUT</button>
+  </div>
+  <div className="cart-promocode">
+    <div>
+      <p>If you have a promocode, Enter it here</p>
+      <div className='cart-promocode-input'>
+        <input type="text" placeholder='promocode' />
+        <button>Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div>
   )
 }
 
